fd331b801c4f5023cd5e3e9a863e354039acd1d5
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-01-12 18:53:10 -0800

    feat: add snapraid disks

---
+++ nixos/disks-snapraid.nix
@@ -0,0 +1,21 @@
+_: {
+  fileSystems."/mnt/pari1" = {
+    device = "/dev/disk/by-id/ata-WDC_WD40EFAX-68JH4N0_WD-WX62D10LE1SE";
+    fsType = "ext4";
+  };
+
+  fileSystems."/mnt/disk1" = {
+    device = "/dev/disk/by-id/ata-WDC_WD40EFAX-68JH4N0_WD-WX62D10LEJHE";
+    fsType = "ext4";
+  };
+
+  fileSystems."/mnt/disk2" = {
+    device = "/dev/disk/by-id/ata-WDC_WD40EFAX-68JH4N0_WD-WX62D10LEKV3";
+    fsType = "ext4";
+  };
+
+  fileSystems."/mnt/disk3" = {
+    device = "/dev/disk/by-id/ata-WDC_WD40EFAX-68JH4N0_WD-WX62D10LEN1A";
+    fsType = "ext4";
+  };
+}
