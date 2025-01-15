d9dbba366328b52c3abac0ab57ee9d51ba210eb8
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-01-14 16:17:52 -0800

    fix: actually hook up services

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,6 +6,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./disks-snapraid.nix
+    ./services.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -20,10 +21,6 @@
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   # console = {
@@ -31,31 +28,6 @@
   #   keyMap = "us";
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  services.tailscale = {
-    enable = true;
-  };
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
   users.defaultUserShell = pkgs.zsh;
 
@@ -89,9 +61,6 @@
   programs.zsh.enable = true;
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
--- nixos/services.nix
+++ nixos/services.nix
@@ -1,6 +1,16 @@
 {...}: {
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
   services.taskchampion-sync-server = {
     enable = true;
     port = 10222;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "us";
+
+  services.tailscale = {
+    enable = true;
   };
 }
