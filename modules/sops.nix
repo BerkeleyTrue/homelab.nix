653905ae62a66627c071b49f61dbe722d9f249ac
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-01-18 18:44:31 -0800

    fix(traefik): just use full path.

--- modules/sops.nix
+++ modules/sops.nix
@@ -2,9 +2,9 @@
   sops.defaultSopsFile = ../secrets/secrets.yml;
   sops.age = {
     sshKeyPaths = [
-      "~/.ssh/id_ed25519"
+      "/home/bt/.ssh/id_ed25519"
     ];
-    keyFile = "~/.config/sops/age/keys.txt";
+    keyFile = "/home/bt/.config/sops/age/keys.txt";
   };
   sops.secrets.cloudflare_email = {};
 }
