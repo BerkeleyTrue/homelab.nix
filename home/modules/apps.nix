fc898c3ad9c20b26fc0c51635d9ab0dfe71d94e0
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-05-03 18:31:34 -0700

    fix: remove old gotas dep

--- home/modules/apps.nix
+++ home/modules/apps.nix
@@ -1,22 +1,4 @@
-{
-  pkgs,
-  ...
-}: let
-  gotas = pkgs.buildGoModule {
-    pname = "gotas";
-    version = "0.1.1";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "szaffarano";
-      repo = "gotas";
-      rev = "12933a2f5a1eaa07b520b96184198724706a6ab6";
-      hash = "sha256-VduHspo/DnZN8ux9qcpi9jefNUBGOdzZmhbm1gYxG60=";
-    };
-
-    vendorHash = "sha256-s7P9cproB1gxjAwpS6NPMOb/rIaNLZQjoTToB7jvOuc=";
-  };
-in {
+{...}: {
   home.packages = [
-    gotas
   ];
 }
