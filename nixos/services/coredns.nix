effa05bcce97155959e1f411126eaa9296e72891
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-07-30 18:08:21 -0700

    fix(coredns): bad regex for subdomain, no quotes

--- nixos/services/coredns.nix
+++ nixos/services/coredns.nix
@@ -24,7 +24,7 @@ r3dm.com:${portString} {
     expr incidr(client_ip(), '10.6.0.0/16')
   }
   template IN A {
-    match "^r3dm.com$"
+    match .*\.r3dm.com$
     answer "{{.Name}} 60 IN A 10.6.7.10"
   }
 }
@@ -39,7 +39,7 @@ r3dm.com:${portString} {
   }
 
   template IN A {
-    match "^r3dm.com$"
+    match .*\.r3dm.com$
     answer "{{.Name}} 60 IN A 10.6.7.10"
   }
 }
