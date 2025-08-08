f4214cd0f9dc228cae04c254fc27775ced95aef7
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-08-08 12:10:28 -0700

    fix(flake): remove boulder, use just

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
     home-manager-parts.url = "github:berkeleytrue/home-manager-parts";
-    boulder.url = "github:berkeleytrue/nix-boulder-banner";
   };
 
   outputs = inputs @ {
@@ -28,15 +27,10 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
       imports = [
-        inputs.boulder.flakeModule
         inputs.home-manager-parts.flakeModule
         ./home
       ];
-      perSystem = {
-        config,
-        system,
-        ...
-      }: let
+      perSystem = {system, ...}: let
         pkgs = import inputs.nixpkgs {
           inherit system;
 
@@ -49,70 +43,27 @@
             ];
           };
         };
-
-        home-uri = ".\#homeConfigurations.homelab";
-        activation-package-uri = "${home-uri}.activationPackage";
-
-        nixos-build = pkgs.writeShellScriptBin "nixos-build" ''
-          # check if nixos-rebuild is available
-          sudo nixos-rebuild switch --print-build-logs --show-trace --flake .
-        '';
-
-        nixos-dryrun = pkgs.writeShellScriptBin "nixos-dryrun" ''
-          # check if nixos-rebuild is available
-          nixos-rebuild dry-run --print-build-logs --show-trace --flake .
-        '';
-
-        home-manager-build = pkgs.writeShellScriptBin "home-manager-build" ''
-          # check if home-manager is available
-          nix build \
-            --show-trace \
-            --no-link \
-            --print-build-logs \
-            ${activation-package-uri} &&
-            nix flake show . && \
-            nix path-info ${activation-package-uri}
-        '';
-
-        get-home-manager-path = pkgs.writeShellScriptBin "get-home-manager-path" ''
-          nix path-info ${activation-package-uri}.
-        '';
-
-        home-manager-switch = pkgs.writeShellScriptBin "home-manager-switch" ''
-          eval "$(${get-home-manager-path}/bin/get-home-manager-path)/home-path/bin/home-manager switch --flake ."
-        '';
       in {
         formatter = pkgs.alejandra;
         _module.args.pkgs = pkgs;
 
-        boulder.commands = [
-          {
-            category = "system";
-            description = "Switch NixOS";
-            exec = nixos-build;
-          }
-          {
-            category = "system";
-            description = "Dry run NixOS";
-            exec = nixos-dryrun;
-          }
-          {
-            category = "user";
-            description = "build Home Manager";
-            exec = home-manager-build;
-          }
-          {
-            category = "user";
-            description = "switch Home Manager";
-            exec = home-manager-switch;
-          }
-        ];
-
         devShells.default = pkgs.mkShell {
-          inputsFrom = [config.boulder.devShell];
+          name = "homelab";
           buildInputs = with pkgs; [
             just
           ];
+          shellHook = ''
+            function menu () {
+              echo
+              echo -e "\033[1;34m>==> ️  '$name'\n\033[0m"
+              ${pkgs.just}/bin/just --list
+              echo
+              echo "(Run 'just --list' to display this menu again)"
+              echo
+            }
+
+            menu
+          '';
         };
       };
       flake = {
