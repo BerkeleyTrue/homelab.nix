bb445844e679d34b702b4c19bdd2e0962ee6bb50
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-09-29 19:45:56 -0700

    fix(audiobookshelf): ensure datadir exists

--- nixos/services/audiobookshelf.nix
+++ nixos/services/audiobookshelf.nix
@@ -1,13 +1,15 @@
 {...}: let 
+  traefik_public_url = "r3dm.com";
   host = "0.0.0.0";
   port = 33299;
   user = "audiobookshelf";
   group = "audiobookshelf";
+  dataDir = "/mnt/storate/audiobookshelf";
 in {
   services.audiobookshelf = {
     enable = true;
 
-    dataDir = "/mnt/storate/audiobookshelf";
+    dataDir = dataDir;
     host = host;
     port = port;
     user = user;
@@ -25,7 +27,7 @@ in {
   services.traefik.dynamicConfigOptions.http.routers.audiobookshelf = {
     entrypoints = ["web"];
     service = "audiobookshelf";
-    rule = "Host(`audiobookshelf.r3dm.com`)";
+    rule = "Host(`audiobookshelf.${traefik_public_url}`)";
 
     middlewares = ["ssl-redirect" "ssl-header"];
   };
@@ -33,9 +35,14 @@ in {
   services.traefik.dynamicConfigOptions.http.routers.audiobookshelf-secure = {
     entrypoints = ["secureweb"];
     service = "audiobookshelf";
-    rule = "Host(`audiobookshelf.r3dm.com`)";
+    rule = "Host(`audiobookshelf.${traefik_public_url}`)";
 
     middlewares = ["default-headers"];
     tls.certResolver = "letsencrypt";
   };
+
+  # Ensure the data directory exists
+  systemd.tmpfiles.rules = [
+    "d ${dataDir} 0755 ${user} ${group} -"
+  ];
 }
